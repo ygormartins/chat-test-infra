57205eaf4b6880e97c84bdddab7aa1077dc7a832
ygormartins <ygormartinsr@outlook.com>
2022-12-15 23:16:39 -0300

    feature: changing initials lib to my own and fixing iter issue

--- src/triggers/cognito-post-confirmation.rs
+++ src/triggers/cognito-post-confirmation.rs
@@ -1,13 +1,15 @@
 /*---------- Imports ----------*/
 use aws_lambda_events::cognito::CognitoEventUserPoolsPostConfirmation;
 use aws_sdk_s3::types::ByteStream;
-use image::{ImageBuffer, ImageOutputFormat, Rgba};
-use initials::{AvatarBuilder, AvatarResult};
+use image::ImageOutputFormat;
+use initials_revamped::{AvatarBuilder, AvatarResult};
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use std::{env, io::Cursor};
 
 /*---------- Constants ----------*/
 const IMAGE_SIZE: u32 = 512;
+const FONT_COLOR: &str = "#ffffff";
+const FONT_SCALE: f32 = 250.0;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -23,9 +25,8 @@ async fn main() -> Result<(), Error> {
 
 fn avatar(user_name: &str) -> AvatarResult {
     AvatarBuilder::new(user_name)
-        .with_font_scale(250.0)?
-        .with_font_color("#ffffff")?
-        .with_font("/assets/fonts/Roboto.ttf")?
+        .with_font_scale(FONT_SCALE)?
+        .with_font_color(FONT_COLOR)?
         .with_width(IMAGE_SIZE)?
         .with_height(IMAGE_SIZE)
 }
@@ -35,33 +36,29 @@ async fn handler_fn(
     bucket_name: &str,
     event: LambdaEvent<CognitoEventUserPoolsPostConfirmation>,
 ) -> Result<CognitoEventUserPoolsPostConfirmation, Error> {
-    let user_attributes_iter = &mut event.payload.request.user_attributes.iter();
-
-    let user_sub = user_attributes_iter.find(|attr| attr.0 == "sub");
-    let user_name = user_attributes_iter.find(|attr| attr.0 == "name");
+    let user_sub = event.payload.request.user_attributes.get("sub");
+    let user_name = event.payload.request.user_attributes.get("name");
 
     if let (Some(sub_value), Some(name_value)) = (user_sub, user_name) {
-        let avatar = avatar(name_value.1);
+        let avatar = avatar(name_value);
 
         if let Ok(avatar_result) = avatar {
-            let image_key = format!("user/{}.png", sub_value.1);
-            let image_content = avatar_result.draw().to_vec();
-            let image_buffer: Option<ImageBuffer<Rgba<u8>, Vec<u8>>> =
-                ImageBuffer::from_vec(IMAGE_SIZE, IMAGE_SIZE, image_content);
+            let image_key = format!("user/{}.png", sub_value);
+            let avatar_image = avatar_result.draw();
 
-            if let Some(buf_result) = image_buffer {
-                let mut bytes: Vec<u8> = Vec::new();
-                buf_result.write_to(&mut Cursor::new(&mut bytes), ImageOutputFormat::Png)?;
+            let mut image_bytes: Vec<u8> = Vec::new();
+            let mut image_cursor = Cursor::new(&mut image_bytes);
 
-                s3_client
-                    .put_object()
-                    .bucket(bucket_name)
-                    .key(image_key)
-                    .content_type("image/png")
-                    .body(ByteStream::from(bytes))
-                    .send()
-                    .await?;
-            }
+            avatar_image.write_to(&mut image_cursor, ImageOutputFormat::Png)?;
+
+            s3_client
+                .put_object()
+                .bucket(bucket_name)
+                .key(image_key)
+                .content_type("image/png")
+                .body(ByteStream::from(image_bytes))
+                .send()
+                .await?;
         }
     }
 
